bd806b81285225480d8b88c94f2e8f830ee4b600
jayden-dang <jayden@jaydendang.com>
2024-07-17 20:41:13 +0700

    initial project amaproof

--- projects/ama3-contracts/smart_contracts/ama_proof/contract.py
+++ projects/ama3-contracts/smart_contracts/ama_proof/contract.py
@@ -1,8 +1,80 @@
-from algopy import ARC4Contract, String
+from algopy import ARC4Contract, Account, Asset, String, Txn, UInt64, subroutine
+import algopy
 from algopy.arc4 import abimethod
 
-
 class AmaProof(ARC4Contract):
+    # Function -> ProofOfViewer (time, 50) -> whitelist (address, nft)
+    # Mint NFT -> Wallet -> address -> nft (asset.id)
+    # Oracle Front-end -> Claim POV(ProofOfViewer) Token NFT
+
+    def __init__(self) -> None:
+        self.max_viewer = UInt64(30)
+        self.asset_url = String("ipfs://...0xla")
+        self.total_viewer = UInt64(0)
+        self.winner = Account()
+
+    # Todo -> Who is winner
+    # ko dung vao
+    def choose_winner(self, addr: Account) -> None:
+        pass
+
+    # -->>> Region:: START  --->>>  Confirm
     @abimethod()
-    def hello(self, name: String) -> String:
-        return "Hello, " + name
+    def confirm_joined(self) -> None:
+        # Assert Require
+        assert self.total_viewer < self.max_viewer, "Maximum"
+        # Execute
+        minted_asset = self._mint_pov(Txn.sender)
+        # self.total_viewer = self.total_viewer + 1
+        self.total_viewer += 1
+
+        # Luu tru thong tin nguoi tham gia
+        # mapping(address -> asset.id)
+        # address -> value
+        _id, already_exists = algopy.op.Box.get(Txn.sender.bytes) # address -> bytes
+        # true -> not true -> false -> error
+        assert not already_exists, "Already claim POV"
+
+        # input address -> Box
+        algopy.op.Box.put(Txn.sender.bytes, algopy.op.itob(minted_asset.id))
+
+    # <<<-- Region:: END    <<<---  Confirm
+
+    # -->>> Internal:: START  --->>>  Function Mint NFT
+    @subroutine
+    def _mint_pov(self, claimer: Account) -> Asset:
+        # ASA -> Algorand Standard Assets
+        algopy.ensure_budget(UInt64(10000), algopy.OpUpFeeSource.AppAccount)
+        asset_name = b"Openedu Algorand #" + claimer.bytes
+        # submit -> tao NFT
+        return (
+            algopy.itxn.AssetConfig(
+                asset_name=asset_name,
+                unit_name=String("EDU"),
+                url=self.asset_url,
+                manager=claimer,
+                total=UInt64(1),
+                decimals=0
+            ).submit().created_asset # asset.id
+        )
+    # <<<-- Internal:: END    <<<---  Function Mint NFT
+
+    @abimethod
+    def get_pov_id(self) -> UInt64:
+        # get -> op.Box.get(sender) -> asset.id
+        pass
+        return UInt64(0)
+
+    @abimethod
+    def claim_pov_token(self) -> None:
+        # tao NFT -> AssetConfig -> asset - Reward.
+        # winner == Txn.sender -> NFT
+        # require
+        pass
+
+    @abimethod
+    def send_pov_token(self) -> None:
+        # itxn.AssetTransfer(
+        # asset_id, from, to, amount
+        # )
+        pass
--- projects/ama3-contracts/tests/ama_proof_test.py
+++ projects/ama3-contracts/tests/ama_proof_test.py
@@ -1,25 +1,35 @@
 from collections.abc import Generator
 
+import algopy
+from algopy.op import itob
 import pytest
 from algopy_testing import AlgopyTestContext, algopy_testing_context
 
 from smart_contracts.ama_proof.contract import AmaProof
 
 
+# khoi tao moi truong va ngu canh
 @pytest.fixture()
 def context() -> Generator[AlgopyTestContext, None, None]:
     with algopy_testing_context() as ctx:
         yield ctx
         ctx.reset()
 
-
-def test_hello(context: AlgopyTestContext) -> None:
-    # Arrange
-    dummy_input = context.any_string()
+def test_init(context: AlgopyTestContext) -> None:
     contract = AmaProof()
+    contract.__init__()
 
-    # Act
-    output = contract.hello(dummy_input)
+    assert contract.max_viewer == algopy.UInt64(30)
+    assert contract.total_viewer == algopy.UInt64(0)
+    assert contract.asset_url == algopy.String("ipfs://...0xla")
 
-    # Assert
-    assert output == f"Hello, {dummy_input}"
+def test_confirm_joined(context: AlgopyTestContext) -> None:
+    contract = AmaProof()
+    contract.__init__()
+
+    contract.confirm_joined()
+    # address = context.any_account;
+
+    # fuzzing testing
+    assert context.get_box(context.default_creator.bytes) == itob(1)
+    assert contract.total_viewer == algopy.UInt64(1)
